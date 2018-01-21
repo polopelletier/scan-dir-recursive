b4b0d70513a97e9086356126c0a67de96366fb41
Pierre-Luc Pelletier <polopelletier@gmail.com>
2018-01-21 16:39:14 -0500

    Cleanup

--- utils/shouldIgnore.js
+++ utils/shouldIgnore.js
@@ -1,7 +1,5 @@
 const minimatch = require("minimatch");
 
-const chalk = require("chalk");
-
 module.exports = function(filename, ignore) {
 	const match = [];
 
@@ -10,26 +8,16 @@ module.exports = function(filename, ignore) {
 
 		current = ignore[i];
 
-
 		if(filename === current){
 			// Absolute path match
-			logCurrent(filename, current, chalk.red);
 			return true;
 		}else if(filename.endsWith(current)){
 			// Relative path match
-			logCurrent(filename, current, chalk.red);
 			return true;
 		}else if(minimatch(filename, current)){
 			// Glob match
-			logCurrent(filename, current, chalk.red);
 			return true;
 		}
-		logCurrent(filename, current, chalk.green);
 	}
 	return false;
-}
-
-function logCurrent(filename, current, color){
-	const parts = filename.split("scan-dir-recursive");
-	//console.log(color(parts[parts.length - 1]), color(current))
 }
